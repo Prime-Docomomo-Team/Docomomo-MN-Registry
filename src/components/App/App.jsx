f143235d2c1b5ff3167bfca1ded8367397f659bf
Maya Peters <mayampeters@gmail.com>
2023-09-07 22:30:50 -0500

    adds detail page route to app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,6 +19,7 @@ import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
+import Details from '../Details/Details';
 
 import './App.css';
 
@@ -108,6 +109,9 @@ function App() {
               // Otherwise, show the Landing page
               <LandingPage />
             }
+          </Route>
+          <Route exact path="/details">
+            <Details />
           </Route>
 
           {/* If none of the other routes matched, we will show a 404. */}
