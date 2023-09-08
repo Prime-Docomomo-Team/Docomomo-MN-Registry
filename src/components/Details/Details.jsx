82dc3a9a190bbb7c7591a55154ad71c3a1b30191
Maya Peters <mayampeters@gmail.com>
2023-09-07 22:28:38 -0500

    begins MUI styling on details page

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,10 +1,19 @@
+import { Box, Typography } from "@mui/material";
+
 const Details = () => {
+  return (
+    <Box sx={{display: 'flex', flexDirection: 'column'}}>
+      <Box
+        component="img"
+        sx={{ width: 300, margin: auto, borderRadius: 10 }}
+        src="https://www.creativeboom.com/uploads/articles/07/07ce01e00f639f2cb0423886f647bf19bb12c01e_1620.jpg"
+        alt="This is a placeholder image"
+      ></Box>
+      <Box>
+        <Typography component='h2'>Title of Site</Typography>
+      </Box>
+    </Box>
+  );
+};
 
-    return (
-    
-    <div>Details</div>
-
-    )
-  };
-  
-  export default Details;
+export default Details;
