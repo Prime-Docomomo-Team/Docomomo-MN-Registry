0febc79983228705a28d824b08f071fc9fb39bc0
Luke Gartner <lukegartner4@gmail.com>
2023-09-09 19:24:51 -0500

    add get filtered sites route

--- server/routes/sites.router.js
+++ server/routes/sites.router.js
@@ -14,6 +14,36 @@ router.get("/", (req, res) => {
       res.sendStatus(500);
     });
 });
+/**
+ * GET filtered sites
+ */
+// req.body should be an array with objects containing field, and input value
+// const sampleBody = [
+//   { field: "architect", input: "williaM purvy" },
+//   { field: "street", input: "penn" },
+// ];
+router.get("/filtered", (req, res) => {
+  const queryText = `
+  SELECT * FROM sites
+  WHERE $1;`;
+  const whereStatement = req.body
+    .map(
+      (filter) =>
+        `${filter.input
+          .split(" ")
+          .map((input) => `${filter.field} ILIKE '%${input}%'`)
+          .join(" AND ")} `
+    )
+    .join("AND ");
+  const queryArgs = [whereStatement];
+  pool
+    .query(queryText, queryArgs)
+    .then((result) => res.send(result.rows))
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+    });
+});
 
 /**
  * POST route template
