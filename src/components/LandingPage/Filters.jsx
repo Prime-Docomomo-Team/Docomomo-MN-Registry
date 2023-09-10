a0c93457348614bf9053c6d5009d950f6834c0c5
Luke Gartner <lukegartner4@gmail.com>
2023-09-10 14:10:35 -0500

    dynamically renders TextField for filterable columns

--- src/components/LandingPage/Filters.jsx
+++ src/components/LandingPage/Filters.jsx
@@ -1,7 +1,15 @@
-import { useDispatch } from "react-redux";
+import { Button } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Filters = () => {
   const dispatch = useDispatch();
+  const { sitesColumns } = useSelector((store) => store);
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_SITES_COLUMNS" });
+  }, []);
 
   const applyFilters = () => {
     dispatch({
@@ -13,7 +21,27 @@ const Filters = () => {
     });
   };
 
-  return <div onClick={applyFilters}>Filters</div>;
+  console.log("sitesColumns", sitesColumns);
+
+  return (
+    <div>
+      {sitesColumns
+        .filter(
+          (column) =>
+            !["id", "latitude", "longitude", "description"].includes(
+              column.column_name
+            )
+        )
+        .map((column) => (
+          <TextField
+            key={column.ordinal_position}
+            label={column.column_name.replace("_", " ")}
+          />
+        ))}
+      <Button variant="contained">Apply Filters</Button>
+      <Button variant="outlined">Clear Filters</Button>
+    </div>
+  );
 };
 
 export default Filters;
