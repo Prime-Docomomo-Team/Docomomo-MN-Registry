55b0a16772ee9bfbffcec0050b3f60dff35ccebc
Maya Peters <mayampeters@gmail.com>
2023-09-10 20:20:43 -0500

    adds more styling to details page

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Divider } from "@mui/material";
 
 const Details = () => {
   return (
@@ -22,31 +22,82 @@ const Details = () => {
           Title of Site
         </Typography>
       </Box>
-      <Box sx={{display: 'flex', gap: 8}}>
-        <Box>
+      <Divider component="div" variant="presentation" />
+      <Box sx={{ display: "flex", gap: 8 }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            width: "10rem",
+          }}
+        >
           <Typography component="p" variant="p" sx={{ fontWeight: "bold" }}>
             Address:
           </Typography>
-          <Typography component="p" variant="p">
+          <Typography component="p" variant="p" sx={{ textAlign: "center" }}>
             123 Main Street, Saint Paul, MN
           </Typography>
         </Box>
-        <Box>
-          <Typography component="p" variant="p" sx={{ fontWeight: "bold", color: '' }}>
-            Address:
-          </Typography>
-          <Typography component="p" variant="p">
-            123 Main Street, Saint Paul, MN
-          </Typography>
-        </Box>
-        <Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            width: "10rem",
+          }}
+        >
           <Typography component="p" variant="p" sx={{ fontWeight: "bold" }}>
-            Address:
+            Architect:
           </Typography>
           <Typography component="p" variant="p">
-            123 Main Street, Saint Paul, MN
+            Picasso
           </Typography>
         </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            width: "10rem",
+          }}
+        >
+          <Typography
+            component="p"
+            variant="p"
+            sx={{ fontWeight: "bold", color: "secondary" }}
+          >
+            Year Built:
+          </Typography>
+          <Typography component="p" variant="p">
+            1967
+          </Typography>
+        </Box>
+      </Box>
+      <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2}}>
+        <Typography component="p" variant="p" sx={{ fontWeight: "bold" }}>
+          Description
+        </Typography>
+        <Typography component="p" variant="p" sx={{width: '50rem'}}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non leo a
+          lacus lacinia pulvinar id ut ligula. Cras bibendum tellus accumsan,
+          venenatis massa id, posuere velit. Suspendisse auctor leo non justo
+          elementum viverra. Etiam a convallis ipsum, non ultricies ex. Integer
+          ligula urna, feugiat lacinia purus posuere, tempor finibus mauris. Nam
+          sit amet aliquam felis. Nunc ultrices nec diam quis malesuada.
+          Suspendisse non metus diam. Ut feugiat, lacus sit amet facilisis
+          placerat, metus nulla vulputate nunc, et pretium augue nunc vel odio.
+          Pellentesque mollis quam ex, vel porttitor nulla maximus et.
+          Vestibulum quis accumsan sapien. Aliquam mattis nunc aliquam hendrerit
+          pulvinar. Aliquam ut accumsan erat. Aenean ac est lacus. Nam neque
+          urna, ullamcorper vel pellentesque sed, ullamcorper eget ex.
+          Vestibulum rutrum vel est eu sagittis. Etiam elementum sagittis mauris
+          et rhoncus. Vivamus auctor aliquet justo sed faucibus. Donec id odio
+          vel odio posuere rutrum. Maecenas faucibus malesuada turpis fermentum
+          sodales. Nam tincidunt neque eget rhoncus posuere. Aenean tellus
+          ligula, rutrum et volutpat vitae, consectetur efficitur est. Cras a
+          quam id ipsum elementum pellentesque ut eu felis.
+        </Typography>
       </Box>
     </Box>
   );
