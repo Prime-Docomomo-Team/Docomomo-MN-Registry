00b240ca16643c35d8412c2dc06f23dd1751e402
Maya Peters <mayampeters@gmail.com>
2023-09-10 19:33:24 -0500

    adds more styling to details page

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,15 +2,51 @@ import { Box, Typography } from "@mui/material";
 
 const Details = () => {
   return (
-    <Box sx={{display: 'flex', flexDirection: 'column'}}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "wrap",
+        alignItems: "center",
+        gap: 2,
+      }}
+    >
       <Box
         component="img"
-        sx={{ width: 300, margin: auto, borderRadius: 10 }}
+        sx={{ height: 375, margin: "auto", borderRadius: 10 }}
         src="https://www.creativeboom.com/uploads/articles/07/07ce01e00f639f2cb0423886f647bf19bb12c01e_1620.jpg"
         alt="This is a placeholder image"
       ></Box>
       <Box>
-        <Typography component='h2'>Title of Site</Typography>
+        <Typography component="h2" variant="h4">
+          Title of Site
+        </Typography>
+      </Box>
+      <Box sx={{display: 'flex', gap: 8}}>
+        <Box>
+          <Typography component="p" variant="p" sx={{ fontWeight: "bold" }}>
+            Address:
+          </Typography>
+          <Typography component="p" variant="p">
+            123 Main Street, Saint Paul, MN
+          </Typography>
+        </Box>
+        <Box>
+          <Typography component="p" variant="p" sx={{ fontWeight: "bold", color: '' }}>
+            Address:
+          </Typography>
+          <Typography component="p" variant="p">
+            123 Main Street, Saint Paul, MN
+          </Typography>
+        </Box>
+        <Box>
+          <Typography component="p" variant="p" sx={{ fontWeight: "bold" }}>
+            Address:
+          </Typography>
+          <Typography component="p" variant="p">
+            123 Main Street, Saint Paul, MN
+          </Typography>
+        </Box>
       </Box>
     </Box>
   );
