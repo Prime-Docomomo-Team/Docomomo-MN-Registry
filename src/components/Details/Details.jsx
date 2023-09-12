cf98734c0e3174f9ecd07a1a930848ce37197fe9
Maya Peters <mayampeters@gmail.com>
2023-09-12 13:51:00 -0500

    begins implementation of grid in details page

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,7 +1,34 @@
-import { Box, Typography, Divider } from "@mui/material";
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { Box, Grid, Typography, Divider, Button } from "@mui/material";
 
 const Details = () => {
+  const history = useHistory();
+  const siteDetails = useSelector(store => store.details);
+  console.log(siteDetails);
+
   return (
+<>
+    <Box sx={{padding: 2, backgroundColor: '#D4D4D4', width: '75%', margin: 'auto'}}>
+      <Grid container >
+        <Grid item >
+          <Button onClick={()=>history.goBack}>BACK</Button>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={8} md={5} lg={4}>
+          <Box
+          component="img"
+          sx={{ borderRadius: 10 }}
+          src="https://www.creativeboom.com/uploads/articles/07/07ce01e00f639f2cb0423886f647bf19bb12c01e_1620.jpg"
+          alt="This is a placeholder image"
+        ></Box>
+        </Grid>
+      </Grid>
+
+    </Box>
+
+
     <Box
       sx={{
         display: "flex",
@@ -100,6 +127,7 @@ const Details = () => {
         </Typography>
       </Box>
     </Box>
+    </>
   );
 };
 
