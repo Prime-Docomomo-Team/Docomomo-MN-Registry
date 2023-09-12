ea785d02a22aa86ea68931f37e4c55a9ba43b1ab
Maya Peters <mayampeters@gmail.com>
2023-09-12 14:18:40 -0500

    implements MUI grid on details page

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,133 +1,129 @@
-import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { Box, Grid, Typography, Divider, Button } from "@mui/material";
+import TypeOverrides from "pg/lib/type-overrides";
 
 const Details = () => {
   const history = useHistory();
-  const siteDetails = useSelector(store => store.details);
+  const siteDetails = useSelector((store) => store.details);
   console.log(siteDetails);
 
   return (
-<>
-    <Box sx={{padding: 2, backgroundColor: '#D4D4D4', width: '75%', margin: 'auto'}}>
-      <Grid container >
-        <Grid item >
-          <Button onClick={()=>history.goBack}>BACK</Button>
-        </Grid>
-      </Grid>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={8} md={5} lg={4}>
-          <Box
-          component="img"
-          sx={{ borderRadius: 10 }}
-          src="https://www.creativeboom.com/uploads/articles/07/07ce01e00f639f2cb0423886f647bf19bb12c01e_1620.jpg"
-          alt="This is a placeholder image"
-        ></Box>
-        </Grid>
-      </Grid>
-
-    </Box>
-
-
     <Box
       sx={{
-        display: "flex",
-        flexDirection: "column",
-        flexWrap: "wrap",
-        alignItems: "center",
-        gap: 2,
+        padding: 2,
+        backgroundColor: "#D4D4D4",
+        width: "80%",
+        margin: "auto",
       }}
     >
-      <Box
-        component="img"
-        sx={{ height: 375, margin: "auto", borderRadius: 10 }}
-        src="https://www.creativeboom.com/uploads/articles/07/07ce01e00f639f2cb0423886f647bf19bb12c01e_1620.jpg"
-        alt="This is a placeholder image"
-      ></Box>
-      <Box>
-        <Typography component="h2" variant="h4">
-          Title of Site
-        </Typography>
-      </Box>
-      <Divider component="div" variant="presentation" />
-      <Box sx={{ display: "flex", gap: 8 }}>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            width: "10rem",
-          }}
+      <Grid container>
+        <Grid item>
+          <Button onClick={() => history.goBack}>BACK</Button>
+        </Grid>
+      </Grid>
+      <Grid container justifyContent="center">
+        <Grid item xs={12} sm={8} md={6} lg={5}>
+          <Box
+            component="img"
+            sx={{ borderRadius: 10 }}
+            src="https://www.creativeboom.com/uploads/articles/07/07ce01e00f639f2cb0423886f647bf19bb12c01e_1620.jpg"
+            alt="This is a placeholder image"
+          ></Box>
+        </Grid>
+      </Grid>
+      <Grid container padding={2} justifyContent="center">
+        <Grid item>
+          <Typography component="h2" variant="h4">
+            Title of Site
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid container justifyContent="center" display="flex" gap={5}>
+        <Grid
+          item
+          justifyContent="center"
+          display="flex"
+          flexDirection="column"
         >
+          <Grid item>
+            <Typography component="p" sx={{ fontWeight: "bold" }}>
+              Address
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography>123 Main Street, Minneapolis, MN 55418</Typography>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          justifyContent="center"
+          display="flex"
+          flexDirection="column"
+        >
+          <Grid item>
+            <Typography component="p" sx={{ fontWeight: "bold" }}>
+              Architect
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography>Picasso</Typography>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          justifyContent="center"
+          display="flex"
+          flexDirection="column"
+        >
+          <Grid item>
+            <Typography component="p" sx={{ fontWeight: "bold" }}>
+              Year Built
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography>1967</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        display="flex"
+        flexDirection="column"
+        padding={2}
+        gap={2}
+        alignItems="center"
+      >
+        <Grid item>
           <Typography component="p" variant="p" sx={{ fontWeight: "bold" }}>
-            Address:
+            Description
           </Typography>
-          <Typography component="p" variant="p" sx={{ textAlign: "center" }}>
-            123 Main Street, Saint Paul, MN
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            width: "10rem",
-          }}
-        >
-          <Typography component="p" variant="p" sx={{ fontWeight: "bold" }}>
-            Architect:
-          </Typography>
+        </Grid>
+        <Grid item>
           <Typography component="p" variant="p">
-            Picasso
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non leo
+            a lacus lacinia pulvinar id ut ligula. Cras bibendum tellus
+            accumsan, venenatis massa id, posuere velit. Suspendisse auctor leo
+            non justo elementum viverra. Etiam a convallis ipsum, non ultricies
+            ex. Integer ligula urna, feugiat lacinia purus posuere, tempor
+            finibus mauris. Nam sit amet aliquam felis. Nunc ultrices nec diam
+            quis malesuada. Suspendisse non metus diam. Ut feugiat, lacus sit
+            amet facilisis placerat, metus nulla vulputate nunc, et pretium
+            augue nunc vel odio. Pellentesque mollis quam ex, vel porttitor
+            nulla maximus et. Vestibulum quis accumsan sapien. Aliquam mattis
+            nunc aliquam hendrerit pulvinar. Aliquam ut accumsan erat. Aenean ac
+            est lacus. Nam neque urna, ullamcorper vel pellentesque sed,
+            ullamcorper eget ex. Vestibulum rutrum vel est eu sagittis. Etiam
+            elementum sagittis mauris et rhoncus. Vivamus auctor aliquet justo
+            sed faucibus. Donec id odio vel odio posuere rutrum. Maecenas
+            faucibus malesuada turpis fermentum sodales. Nam tincidunt neque
+            eget rhoncus posuere. Aenean tellus ligula, rutrum et volutpat
+            vitae, consectetur efficitur est. Cras a quam id ipsum elementum
+            pellentesque ut eu felis.
           </Typography>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            width: "10rem",
-          }}
-        >
-          <Typography
-            component="p"
-            variant="p"
-            sx={{ fontWeight: "bold", color: "secondary" }}
-          >
-            Year Built:
-          </Typography>
-          <Typography component="p" variant="p">
-            1967
-          </Typography>
-        </Box>
-      </Box>
-      <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2}}>
-        <Typography component="p" variant="p" sx={{ fontWeight: "bold" }}>
-          Description
-        </Typography>
-        <Typography component="p" variant="p" sx={{width: '50rem'}}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non leo a
-          lacus lacinia pulvinar id ut ligula. Cras bibendum tellus accumsan,
-          venenatis massa id, posuere velit. Suspendisse auctor leo non justo
-          elementum viverra. Etiam a convallis ipsum, non ultricies ex. Integer
-          ligula urna, feugiat lacinia purus posuere, tempor finibus mauris. Nam
-          sit amet aliquam felis. Nunc ultrices nec diam quis malesuada.
-          Suspendisse non metus diam. Ut feugiat, lacus sit amet facilisis
-          placerat, metus nulla vulputate nunc, et pretium augue nunc vel odio.
-          Pellentesque mollis quam ex, vel porttitor nulla maximus et.
-          Vestibulum quis accumsan sapien. Aliquam mattis nunc aliquam hendrerit
-          pulvinar. Aliquam ut accumsan erat. Aenean ac est lacus. Nam neque
-          urna, ullamcorper vel pellentesque sed, ullamcorper eget ex.
-          Vestibulum rutrum vel est eu sagittis. Etiam elementum sagittis mauris
-          et rhoncus. Vivamus auctor aliquet justo sed faucibus. Donec id odio
-          vel odio posuere rutrum. Maecenas faucibus malesuada turpis fermentum
-          sodales. Nam tincidunt neque eget rhoncus posuere. Aenean tellus
-          ligula, rutrum et volutpat vitae, consectetur efficitur est. Cras a
-          quam id ipsum elementum pellentesque ut eu felis.
-        </Typography>
-      </Box>
+        </Grid>
+      </Grid>
     </Box>
-    </>
   );
 };
 
