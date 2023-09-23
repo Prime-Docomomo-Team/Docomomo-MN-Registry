11edf3506f14ce804637b548828c21a6df3c67d7
Luke Gartner <lukegartner4@gmail.com>
2023-09-23 18:34:29 -0500

    rewrites basic map funcionality in better performing library

---
+++ src/components/LandingPage/GoogleMap.jsx
@@ -0,0 +1,123 @@
+import { useState, useCallback, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { GoogleMapProvider } from "@ubilabs/google-maps-react-hooks";
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
+import SuperClusterAlgorithm from "./superClusterAlgorithm";
+import ReactDOMServer from "react-dom/server";
+import { Button, Box, Typography } from "@mui/material";
+import { ReactDOM } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
+const mapOptions = {
+  zoom: 6,
+  center: { lat: 44.986656, lng: -93.258133 },
+};
+
+const GoogleMap = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const sites = useSelector((store) => store.sites);
+  const [mapContainer, setMapContainer] = useState(null);
+  const [markerCluster, setMarkerCluster] = useState(null);
+  const [map, setMap] = useState(null);
+  const onLoad = useCallback(
+    (map) => {
+      addMarkers(map);
+      setMap(map);
+    },
+    [sites]
+  );
+
+  useEffect(() => {
+    console.log("markerCLuster", markerCluster);
+    console.log("map", map);
+    if (map && markerCluster) {
+      addMarkers(map);
+    }
+  }, [sites]);
+
+  const addMarkers = (map) => {
+    if (markerCluster) {
+      markerCluster.clearMarkers();
+    }
+    const infoWindow = new google.maps.InfoWindow();
+
+    const markers = sites.map((site) => {
+      const marker = new google.maps.Marker({
+        position: { lat: site.latitude, lng: site.longitude },
+      });
+
+      marker.addListener("click", () => {
+        const pushHistory = () => {
+          console.log("hi");
+          history.push(`/details/${site.id}`);
+          dispatch({ type: "SET_SITES", payload: [] });
+        };
+        const infoWindowContent = (
+          <div>
+            <h5>{site.site_name}</h5>
+            <p>
+              {site.street}, {site.city}, {site.state}, {site.zip}
+            </p>
+            {site.url_id != null && (
+              <img
+                src={`https://drive.google.com/uc?export=view&id=${site.url_id}`}
+                style={{ height: 100, margin: "auto" }}
+              ></img>
+            )}
+
+            <a href={`../#/details/${site.id}`}>Read More</a>
+          </div>
+        );
+
+        infoWindow.setPosition({ lat: site.latitude, lng: site.longitude });
+        infoWindow.setContent(ReactDOMServer.renderToString(infoWindowContent));
+        infoWindow.open({ map });
+        // google.maps.event.addListener(infoWindow, "domready", function (e) {
+        //   ReactDOM.render(infoWindowContent, infoWindow);
+        // });
+      });
+
+      return marker;
+    });
+
+    const newMarkerCluster = new MarkerClusterer({
+      markers,
+      map,
+      algorithm: new SuperClusterAlgorithm({ radius: 100 }),
+    });
+
+    setMarkerCluster(newMarkerCluster);
+  };
+
+  return sites && sites.length > 0 ? (
+    <GoogleMapProvider
+      googleMapsAPIKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+      options={mapOptions}
+      mapContainer={mapContainer}
+      onLoad={onLoad}
+    >
+      <div className="map-container" ref={(node) => setMapContainer(node)} />
+    </GoogleMapProvider>
+  ) : (
+    <div className="map-container"></div>
+  );
+};
+
+// function addMarkers(map, sites) {
+//   const markers = sites.map((site) => {
+//     const marker = new google.maps.Marker({
+//       position: { lat: site.latitude, lng: site.longitude },
+//     });
+
+//     return marker;
+//   });
+
+//   new MarkerClusterer({
+//     markers,
+//     map,
+//     algorithm: new SuperClusterAlgorithm({ radius: 100 }),
+//   });
+// }
+
+export default GoogleMap;
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import GoogleMap from "./GoogleMap";
 import "./LandingPage.css";
-import { useLoadScript } from "@react-google-maps/api";
 
 // CUSTOM COMPONENTS
 import RegisterForm from "../RegisterForm/RegisterForm";
-import Map from "./Map";
 import Filters from "./Filters";
 
 // MUI
@@ -15,9 +14,6 @@ import Box from "@mui/material/Box";
 function LandingPage() {
   const [heading, setHeading] = useState("Welcome");
   const history = useHistory();
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-  });
 
   const onLogin = (event) => {
     history.push("/login");
@@ -25,21 +21,8 @@ function LandingPage() {
 
   return (
     <div className="container">
-      {!isLoaded ? (
-        <Box
-          className="map-container"
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <CircularProgress />
-        </Box>
-      ) : (
-        <Map />
-      )}
-      {isLoaded && <Filters />}
+      <GoogleMap />
+      <Filters />
     </div>
   );
 }
